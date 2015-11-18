f4d5a656b86bcb4e5a87cbf1af1ebcffe34a44df
Øyvind Kjerland <oyvinkje@stud.ntnu.no>
2015-11-18 17:48:51 +0100

    Update MCU with GUI, remove button specific code

--- src/mcu.c
+++ src/mcu.c
@@ -34,6 +34,8 @@
 #include "test.h"
 #include "ebi.h"
 
+#include "gui.h"
+
 uint32_t msTicks;
 
 
@@ -59,37 +61,21 @@ void Delay(uint32_t dlyTicks)
 int main(void)
 {
 
-	/* Use 32MHZ HFRCO as core clock frequency */
-	CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFRCO);
+	CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
 
-	//
-	// TODO Temp for testing on BSP, separate init for mcu needed
-	//
 
-	/* Initialize DK board register access */
 #ifdef DEVKIT
+	/* Initialize DK board register access */
 	BSP_Init(BSP_INIT_DK_SPI);
 	BSP_PeripheralAccess(BSP_MICROSD, true);
 #endif
-	/* If first word of user data page is non-zero, enable eA Profiler trace */
-	//BSP_TraceProfilerSetup();
-	/* Test leds */
-	//BSP_LedsSet(0x1);
-	/* Enable SPI access to MicroSD card */
-	//
-	//
-	//
 
-
-	//BSP_LedsSet(0xff);
 
 	// Initialize FatFS and MicroSD
 	init_filesystem();
 
-	//BSP_LedsSet(0xff);
 	// Initialize EBI bus
-	//init_ebi();
-
+	init_ebi();
 
 	//Initialize buttons and their interrupts
 	init_buttons();
@@ -105,126 +91,21 @@ int main(void)
 	*/
 
 
-	//BSP_LedsSet(0xff);
-/*
-	// Testing Output
-	GPIO_PinModeSet( gpioPortA, 0, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 1, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 2, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 3, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 4, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 5, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 6, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 7, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 8, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 9, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 10, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 11, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 12, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 13, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 14, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 15, gpioModePushPull, 0 );
+	// Initialize GUI
+	init_gui();
+	Start_SlaveSerial();
 
-
-	int i = 0;
-	for (i=0; i < 16; i++) {
-		GPIO_PinOutClear(gpioPortA, i);
-	}
-	GPIO_PinOutSet(gpioPortA, 10); //2nd bit
-
-	GPIO_PinModeSet( gpioPortA, 14, gpioModePushPull, 0 );
-	GPIO_PinModeSet( gpioPortA, 15, gpioModePushPull, 0 );
-
-	GPIO_PinModeSet(gpioPortB, 12, gpioModeInputPullFilter, 1); // DONE
-	GPIO_PinModeSet(gpioPortB, 11, gpioModePushPull, 0); // Program B
-	GPIO_PinModeSet(gpioPortB, 8, gpioModeInputPullFilter, 1); // INIT_B
-
-
-
-	int init_b = 0;
-	int program_b = 0;
-	program_b = GPIO_PinInGet(gpioPortB, 11);
-
-	//while(1);
-
-	// Set program_b high until init_b is high
-	GPIO_PinOutSet(gpioPortB, 11);
-	while(init_b == 0) {
-		init_b = GPIO_PinInGet(gpioPortB, 8);
-	}
-
-	// Set program_b to low until init_b is low
-	GPIO_PinOutClear(gpioPortB, 11);
-	while (!(init_b == 0)) {
-		init_b = GPIO_PinInGet(gpioPortB, 8);
-	}
-
-	// Set program_b back to high and wait for init_b is high
-	GPIO_PinOutSet(gpioPortB, 11);
-	while(init_b == 0) {
-		init_b = GPIO_PinInGet(gpioPortB, 8);
-	}
+	//
+	//test_ebi();
+	//test_ebi_buffer();
 
 	int numFiles = 0;
 	char strings[FILE_COUNT][FILENAME_LENGTH];
-	get_filenames("./binfile",strings, &numFiles);
+	get_filenames("./kernel",strings, &numFiles);
 
-
-	Start_SlaveSerial();
-*/
 	while (1) {
 		//EMU_EnterEM2();
 	}
 
-
-
 }
 
-void GPIO_ODD_IRQHandler(void) {
-  /* clear flag for PE1 interrupt */
-  GPIO_IntClear(0xff);
-  interrupt_handler();
-}
-
-void GPIO_EVEN_IRQHandler(void) {
-  /* clear flag for PE1 interrupt */
-  GPIO_IntClear(0xff);
-  interrupt_handler();
-}
-
-void interrupt_handler() {
-
-	if (GPIO_PinInGet(gpioPortD, 5) == 0) {
-		//Switch between SD and camera
-	}
-	else if (GPIO_PinInGet(gpioPortD, 6) == 0) {
-		//Change kernel
-	}
-	else if (GPIO_PinInGet(gpioPortD, 7) == 0) {
-		//Flash FPGA
-	}
-	else if (GPIO_PinInGet(gpioPortD, 8) == 0) {
-		//Mystery function
-	}
-}
-
-void init_buttons() {
-	/* Enable clock for GPIO module */
-	CMU_ClockEnable(cmuClock_GPIO, true);
-
-	/* Configure PE1 as input with filter enable */
-	GPIO_PinModeSet(gpioPortD, 5, gpioModeInputPullFilter, 1);
-	GPIO_PinModeSet(gpioPortD, 6, gpioModeInputPullFilter, 1);
-	GPIO_PinModeSet(gpioPortD, 7, gpioModeInputPullFilter, 1);
-	GPIO_PinModeSet(gpioPortD, 8, gpioModeInputPullFilter, 1);
-
-	/* Enable GPIO_ODD interrupt vector in NVIC */
-	NVIC_EnableIRQ(GPIO_ODD_IRQn);
-	NVIC_EnableIRQ(GPIO_EVEN_IRQn);
-
-	/* Configure PE1 interrupt on falling edge */
-	GPIO_IntConfig(gpioPortD, 5, false, true, true);
-	GPIO_IntConfig(gpioPortD, 6, false, true, true);
-	GPIO_IntConfig(gpioPortD, 7, false, true, true);
-	GPIO_IntConfig(gpioPortD, 8, false, true, true);
-}
